8ff98da91f8e2ceb7e14800929b9e0f51f91233c
felipetojal <153757693+felipetojal@users.noreply.github.com>
2024-04-08 23:33:18 -0300

    Update projeto.js

--- projeto.js
+++ projeto.js
@@ -52,3 +52,50 @@ const contarMedalhasGymnastics = async (tipoMedalha) => {
         return 'Erro ao ler o arquivo CSV.';
     }
 };
+
+// Função para pagina 1, contar quantos atletas chineses participaram de certa olimpíada
+
+const quantidadeChineses = async (anoOlimpiada) => {
+    try {
+        const atletasChineses = await lerAtletasChineses('athlete_events.csv')
+        //Aqui vai filtrar a lista de atletas chineses conforme o ano selecionado
+        const quantidade = atletasChineses
+            .filter(linha => linha.Year == anoOlimpiada)
+            .map(linha => linha.ID)
+            .reduce((total, id) => {
+                if (!total.includes(id)) {
+                    return [...total, id]
+                }
+                return total
+            })
+        const resultado = document.getElementById('resultado')
+        resultado.textContent = `Quantidade de atletas chineses na olimpiada de ${anoOlimpiada}: ${quantidade.length}`
+        return quantidade.length
+    } catch (error) {
+        console.error('Erro ao ler o arquivo csv', error)
+        return `Erro ao ler o arquivo csv`
+    }
+}
+
+// Função para calcular a média de idade
+
+const mediaDeIdade = async (medalhaEsporte) => {
+    try {
+        const atletasChi = await lerAtletasChineses('athlete_events.csv') // Esperando o processamento da função para ler
+        const atletasComMedalha = atletasChi
+            .filter(linha => linha.Medal == medalhaEsporte && linha.Sport == 'Shooting')
+        if (atletasComMedalha.length == 0) {
+            console.log("Erro")
+        }
+        const somaIdades = atletasComMedalha.reduce((acumulador, atleta) => {
+            return acumulador + parseInt(atleta.age, 10) // Usada para converter os inteiros na base decimal 
+        }, 0)
+        const mediaIdade = somaIdades / atletasComMedalha.length
+        const resultado = document.getElementById('resultado')
+        resultado.textContent = `Media de idade: ${mediaIdade}`
+        return mediaIdade
+    } catch (error) {
+        console.error("Erro ao calcular a media")
+        throw error
+    }
+}
